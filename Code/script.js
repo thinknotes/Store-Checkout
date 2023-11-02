62795240e8010ad79e183d9decc30ad523e11123
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-02 10:27:06 -0700

    Created files and started work on JS

---
+++ Code/script.js
@@ -0,0 +1,84 @@
+const items = {
+    "689145740844":{
+        name: "JavaScript Textbook", 
+        price: 34.95
+    },
+
+
+   "4549292070248": {
+       name: "Xerox Paper",
+       price: 10.99
+   },
+   "092265222983": {
+    name: "First Aid Kit", 
+    price: 20.99
+   },
+   "X002ELVL3J": {
+    name: "Box of Pencils (50ct.)", 
+    price: 15.99
+   },
+   "686024002468": {
+    name: "Sanitizing Wipes", 
+    price: 10.99
+
+   }, 
+   "860004186236": {
+    name: "N95 Face Masks", 
+    price: 15.99
+
+   }, 
+   "036000214000": {
+    name: "Kleenex",
+    price: 3.99
+
+   }, 
+   "": {
+    
+   }, 
+   
+}
+
+const Sanitizer = {
+    name: "Hand Sanitizer",
+    number: 8809693254156,
+    price: 7.99
+}
+
+const Printer = {
+    name: "Printer Paper",
+    number: 036500060480,
+    price: 9.99
+}
+
+const Brush = {
+    name: "Brush Pens",
+    number: 085014561877,
+    price: 10.99
+}
+
+const Adapter = {
+    name: "Multiport Adapter",
+    number: "X0032YGP2T",
+    price: 25.99
+}
+
+const Scissors = {
+    name: "Scissors (20ct.)",
+    number: "B07G6JT1XS",
+    price: 23.99
+}
+
+const ProgrammingTextbook = {
+    name: "iOS Programming Textbook", 
+    number: 9780134682334,
+    price: 119.99
+}
+
+const Notebook = {
+    name: "Spiral Notebook", 
+    number: 718103230759,
+    price: 1.99
+}
+
+const pro_list = [Textbook, XeroxPaper, FirstAddKit, Pencils, Wipes, FaceMask, Kleenex, Sanitizer, Printer, Brush, Adapter, Scissors, ProgrammingTextbook, Notebook]
+console.log(pro_list[Notebook])
